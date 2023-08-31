865c092fa0a34ffe6ccf890f75146cfc99911bf5
Vinicius Moura Rodrigues <99925877+VinRodrigues@users.noreply.github.com>
2023-08-31 12:07:11 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ while (intencao[0]['intent'] != "despedida"):
 
     #  intenção "etapas" e suas dependências
     if intencao[0]['intent'] == "etapas":
-        pergunta_etapas = input("Você quer saber mais sobre qual Fase (Fase 1, Fase 2, ou Fase 3)?\n")
-        if pergunta_etapas == "etapa1":
+        pergunta_etapas = input("Existem 3 fases, você quer saber mais sobre qual Fase : \n 1)Fase 1 \n 2)Fase 2 \n 3)Fase 3?\n")
+        if pergunta_etapas == "1":
             resposta = "Fase 1: Verificação da viabilidade técnico-científica da proposta (até 9 meses de duração e orçamento de até R$ 300.000, mais Reserva Técnica e Benefícios Complementares)."
-        elif pergunta_etapas == "etapa2":
+        elif pergunta_etapas == "2":
             resposta = "Fase 2: Execução da pesquisa propriamente dita (até 24 meses de duração e orçamento de até R$ 1.500.000, mais Reserva Técnica e Benefícios Complementares)."
-        elif pergunta_etapas == "etapa3":
+        elif pergunta_etapas == "3":
             resposta = "Fase 3: Desenvolvimento comercial e industrial dos produtos, processos, sistemas e/ou serviços inovadores obtidos a partir de pesquisas anteriores realizadas pela pequena empresa."
         else:
             resposta = "Desculpe, não entendi qual fase você deseja saber mais."
